9ed732315e177ad5fe101d3bf6152727a75f00f7
djsabando <douglas_sabando@hotmail.es>
2020-08-13 21:29:20 -0500

    Refactoring-ExtractClass
    CodeSmell
    -Large Class
    Refactoring
    -Extract Class

---
+++ src/modelos/persona.java
@@ -0,0 +1,15 @@
+
+package modelos;
+
+import java.util.ArrayList;
+
+
+public class persona {
+    public String nombre;
+    public String apellido;
+    public int edad;
+    public String direccion;
+    public String telefono;
+    public ArrayList<Paralelo> paralelos;
+    
+}
